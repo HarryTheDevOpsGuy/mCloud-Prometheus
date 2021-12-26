1007ea8fe902e5aa5d8b8aba331e910af1b73155
Harry <mr.harry41@gmail.com>
2021-12-26 07:55:48 +0530

    test

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 
-web: /app/bin/prometheus --config.file=$HOME/prometheus.yml --web.enable-admin-api --web.listen-address=:$PORT
+web: bash run.sh && /app/bin/prometheus --config.file=$HOME/prometheus.yml --web.enable-admin-api --web.listen-address=:$PORT
--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 PROMETHEUS_VERSION="v2.32.1"
 PROM_DIR="prometheus-${PROMETHEUS_VERSION}.linux-amd64"
 
-echo "Downloading latest Jenkins.war file"
-#curl -sL https://github.com/prometheus/prometheus/releases/download/${PROMETHEUS_VERSION}/${PROM_DIR}.tar.gz | tar -xz
+echo "Downloading latest Prometheus file"
+echo curl -sL https://github.com/prometheus/prometheus/releases/download/${PROMETHEUS_VERSION}/${PROM_DIR}.tar.gz | tar -xz
 #mv ${PROM_DIR} bin
