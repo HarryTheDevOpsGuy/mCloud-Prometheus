740995d90d2e60095bc2802bc93a38ca10097a4c
Harry <mr.harry41@gmail.com>
2021-12-26 08:15:10 +0530

    test

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ PROMETHEUS_VERSION="2.32.1"
 PROM_DIR="prometheus-${PROMETHEUS_VERSION}.linux-amd64"
 
 echo "Downloading latest Prometheus file"
-echo "curl -sL https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/${PROM_DIR}.tar.gz| tar -xz"
+curl -sL https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/${PROM_DIR}.tar.gz| tar -xz
 mv ${PROM_DIR}/prometheus bin/
 
 ls -la
