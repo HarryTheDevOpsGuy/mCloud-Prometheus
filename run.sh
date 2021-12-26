056e4418c30facf6f59bc60072556d3ea273ab27
Harry <mr.harry41@gmail.com>
2021-12-26 08:22:31 +0530

    test

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ echo "Downloading latest Prometheus file"
 curl -sL https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/${PROM_DIR}.tar.gz| tar -xz
 mv ${PROM_DIR}/prometheus bin/
 
+ls -la bin/
 ls -la
